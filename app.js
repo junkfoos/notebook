b988888642ffb21e1101e1ceaf746af2f2723c98
junkfoos <43782062+junkfoos@users.noreply.github.com>
2018-10-22 08:41:00 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,37 +2,37 @@ $(document).ready(function() {
  var currentPage = "page1";
     
     $("#save").click(function() {
-        //if () {
-        localStorage.setItem("#page1", $("#notes").val());
-   // }
-                     
-                     
-        localStorage.setItem("#page2", $("#notes").val());
-        localStorage.setItem("#page3", $("#notes").val()); 
+        if (currentPage == "page1") {
+            localStorage.setItem("page1", $("#notes").val());
+        }
+        else if (currentPage == "page2"){
+            localStorage.setItem("page2", $("#notes").val());
+        }           
+        else if (currentPage == "page3") {
+             localStorage.setItem("page3", $("#notes").val()); 
+         }            
         console.log($("#notes").val());  
     
-    });
-    $("#load").click(function() {
-        $("#notes").val(localStorage.getItem("#notes"));
-        $("#notes2").val(localStorage.getItem("#notes"));
-        $("#notes3").val(localStorage.getItem("#notes"));
     });
     $("#p1").click(function(){   
         $("#notes").val(" "); 
         currentPage = "page1";
-        localStorage.setItem("#page1Saved","page1");
+        var saved1 = localStorage.getItem("page1");
+        $("#notes").val(saved1);
         console.log($("#notes").val());    
     });
     $("#p2").click(function(){
         $("#notes").val(" ");
         currentPage = "page2";
-        localStorage.setItem("#page2Saved","page2");
+        var saved2 = localStorage.getItem("page2");
+        $("#notes").val(saved2);
         console.log($("#notes").val());    
     });
     $("#p3").click(function(){
         $("#notes").val(" ");
         currentPage = "page3"
-        localStorage.setItem("#Page3Saved","page3");
+        var saved3 = localStorage.getItem("page3");
+        $("#notes").val(saved3);
         console.log($("#notes").val()); 
     });
     
